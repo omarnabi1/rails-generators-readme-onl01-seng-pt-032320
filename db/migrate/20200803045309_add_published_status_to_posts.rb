d5374b6ce513b734bc95193c9ded6a2498523bab
Omar Nabi <onabi20@gmail.com>
2020-08-04 16:49:41 +0000

    Automatically backed up by Learn

--- db/migrate/20200803045309_add_published_status_to_posts.rb
+++ db/migrate/20200803045309_add_published_status_to_posts.rb
@@ -1,5 +1,5 @@
-,class AddPublishedStatusToPosts < ActiveRecord::Migration[5.0]
+class AddPublishedStatusToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :published_status :string
+    add_column :posts, :published_status, :string
   end
 end
